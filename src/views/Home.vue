8aa868226c09d9a6743f42cb50aea456609c3dc6
luvenan <luvenan@gmail.com>
2022-03-15 18:55:27 +0100

    Added more CSS and html on filter column

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,8 +8,87 @@
   </div>
   <div class="grid-container">
     <div class="filter-column">
-      <p>this is the column</p>
-      
+      <!-- search bar -->
+      <form action="">
+        <div class="tags-container">
+          <li class="tags">#fae</li>
+          <li class="tags">#shape-shifters</li>
+          <li class="tags">#vampires</li>
+          <li class="tags">#zombies</li>
+        </div>
+        
+        <div class="filter-checkboxes">
+          <h3 class="category-title">Category</h3>
+            <div class="checkbox-item">
+            <input type="checkbox" id="urban fantasy" name="urban fantasy" value="urban fantasy">
+            <label for="urban fantasy"> Urban Fantasy</label>
+            </div>
+            
+            <div class="checkbox-item">
+            <input type="checkbox" id="dystopia" name="dystopia" value="dystopia">
+            <label for="dystopia"> Dystopia</label>
+            </div>
+            
+            <div class="checkbox-item">
+            <input type="checkbox" id="sci-fi" name="sci-fi" value="sci-fi">
+            <label for="sci-fi"> Sci-fi</label>
+            </div>
+            
+            <div class="checkbox-item">
+            <input type="checkbox" id="young adult" name="young adult" value="young adult">
+            <label for="young adult"> Young Adult</label>
+            </div>
+
+          <h3 class="category-title">Author</h3>
+            <div class="checkbox-item">
+            <input type="checkbox" id="seannan mcguire" name="seannan mcguire" value="seannan mcguire">
+            <label for="seannan mcguire"> Seannan Mcguire</label>
+            </div>
+            
+            <div class="checkbox-item">
+            <input type="checkbox" id="juliet marillier" name="juliet marillier" value="juliet marillier">
+            <label for="juliet marillier"> Juliet Marillier</label>
+            </div>
+            
+            <div class="checkbox-item">
+            <input type="checkbox" id="faith hunter" name="faith hunter" value="faith hunter">
+            <label for="faith hunter"> Faith Hunter</label>
+            </div>
+            
+            <div class="checkbox-item">
+            <input type="checkbox" id="john scalzi" name="john scalzi" value="john scalzi">
+            <label for="john scalzi"> John Scalzi</label>
+            </div>
+            
+            <div class="checkbox-item">
+            <input type="checkbox" id="ben aaronovitch" name="ben aaronovitch" value="ben aaronovitch">
+            <label for="ben aaronovitch"> Ben Aaronovitch</label>
+            </div>
+
+          <h3 class="category-title">Number of Books Released</h3>
+            <div class="checkbox-item">
+            <input type="checkbox" id="1-3" name="1-3" value="1-3">
+            <label for="1-3"> 1-3</label>
+            </div>
+            
+            <div class="checkbox-item">
+            <input type="checkbox" id="4-7" name="4-7" value="4-7">
+            <label for="4-7"> 4-7</label>
+            </div>
+            
+            <div class="checkbox-item">
+            <input type="checkbox" id="8-15" name="8-15" value="8-15">
+            <label for="8-15"> 8-15</label>
+            </div>
+            
+            <div class="checkbox-item">
+            <input type="checkbox" id="16+" name="16+" value="16+">
+            <label for="16+"> 16+</label>
+            </div>
+          
+            <input class="filter-button" type="submit" value="Filter">
+          </div>
+        </form>
     </div>
     <div class="results-column">
       <CollectionCard />
