7319210d4c6e2fa30978616c2a97c1c24502326c
luvenan <luvenan@gmail.com>
2022-04-26 14:43:37 +0200

    Made all filters functional, including search box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -35,30 +35,12 @@
             </div>
 
           <h3 class="category-title">Number of Books Released</h3>
-          {{ selected.volumes }}
-          {{ selected.arraysVolumes }}
-          {{ testvar }}
-            <div class="checkbox-item">
-            <input type="checkbox" id="1-3" name="1-3" value=3 v-model.number="selected.volumes">
-            <label for="1-3"> 1-3</label>
+
+            <div class="checkbox-item range" v-for="range in volumesRange" :key="range.id">
+              <input type="checkbox" :id="range" :value="range" v-model="selected.range">
+              <label :for="range">{{ ' ' + range.min + '-' + range.max}}</label>
             </div>
-            
-            <div class="checkbox-item">
-            <input type="checkbox" id="4-7" name="4-7" value=7 v-model.number="selected.volumes">
-            <label for="4-7"> 4-7</label>
-            </div>
-            
-            <div class="checkbox-item">
-            <input type="checkbox" id="8-12" name="8-12" value=12 v-model.number="selected.volumes">
-            <label for="8-12"> 8-12</label>
-            </div>
-            
-            <div class="checkbox-item">
-            <input type="checkbox" id="13+" name="13+" value.number=100 v-model.number="selected.volumes">
-            <label for="13+"> 13+</label>
-            </div>
-          
-            <!--This may not be necessary <input class="filter-button" type="submit" value="Filter"> -->
+         
           </div>
         </form>
     </div>
@@ -89,12 +71,35 @@ export default {
       authorsList: [],
       genresList: [],
       testvar: [],
+      //Describes ranges for number of books released
+      volumesRange: [
+        {
+          id: 1,
+          min: 1,
+          max: 3,
+        },
+        {
+          id: 2,
+          min: 4,
+          max: 7,
+        },
+        {
+          id: 3,
+          min: 8,
+          max: 12,
+        },
+        {
+          id: 4,
+          min: 13,
+          max: 20,
+        }
+      ],
       //Variables selected by the user to filter results
       selected: {
         author: [],
         genre: [],
         volumes: [],
-        // arraysVolumes: []
+        range: []
       }
     }
   },
@@ -106,24 +111,37 @@ export default {
   },
 
   computed: {
-    // This is to turn the volumes range into an array, this solution is cluncky so I'm trying to find another way
-    arraysVolumes: function() {
-      this.testvar = new Array (this.selected.volumes.map(range => range.split(', ').map(Number)))
-    }, 
-    filteredResults: function(){
-      return this.results.filter((series) => {
-        //Filters based in selected authors
-        if (this.selected.author.length > 0 || this.selected.genre.length > 0) {
-          return this.selected.author.includes(series.author) || this.selected.genre.includes(series.genre)
-        } else {
+      filteredResults: function(){
+        let results
+        
+        //Filters the results based on the selected filters - works better by using the else to true because if nothing is filtered then all the results pop up. In the if statements, I check if it doesn't include the selected filter, and then tell it not to show that particular result if it does not. 
+        results = this.results.filter(series => {
+          //Filters by author
+          if (this.selected.author.length > 0) {
+            if (!this.selected.author.includes(series.author)) {
+              return false
+            }
+          }
+          //Filters by genre
+          if (this.selected.genre.length > 0) {
+            if (!this.selected.genre.includes(series.genre)) {
+              return false
+            }
+          }
+          //Filters by number of books released
+          if (this.selected.range.length > 0) {
+            return this.selected.range.map(selRange => series.volumes >= selRange.min && series.volumes <= selRange.max).includes(true)
+          }
+          //Filters by search query
+          if (this.search.length > 0) {
+            if (!series.title.toLowerCase().includes(this.search.toLowerCase()) && !series.author.toLowerCase().includes(this.search.toLowerCase())) {
+              return false
+            }
+          }
           return true
-        }
+        })
 
-        //How to have these both at once? Checks if the the search query is in the series title or author name
-        // if (series.title.toLowerCase().includes(this.search.toLowerCase()) || series.author.toLowerCase().includes(this.search.toLowerCase())) {
-        //   return series
-        // }
-      })
+        return results
     }
   },
   methods: {
